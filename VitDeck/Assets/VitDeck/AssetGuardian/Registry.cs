00f89c41914eb5bb5e2d2d50bbd0760855629e5a
Sokuhatiku <sokuhatiku@gmail.com>
2019-05-06 21:39:28 +0900

    Change registration algorithm to using labels

--- VitDeck/Assets/VitDeck/AssetGuardian/Registry.cs
+++ VitDeck/Assets/VitDeck/AssetGuardian/Registry.cs
@@ -1,4 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using System.Linq;
+using UnityEngine;
 
 namespace VitDeck.AssetGuardian
 {
@@ -7,7 +12,7 @@ namespace VitDeck.AssetGuardian
     /// </summary>
     public static class Registry
     {
-        private static HashSet<string> guardPaths = new HashSet<string>();
+        private const string readonlyLabel = "VitDeck.Readonly";
 
         /// <summary>
         /// パスを保護対象にする。
@@ -15,7 +20,16 @@ namespace VitDeck.AssetGuardian
         /// <param name="path"></param>
         public static void Register(string path)
         {
-            guardPaths.Add(path);
+            var assets = EnumerateAssets(path);
+            foreach (var asset in assets)
+            {
+                if (IsProtected(asset))
+                    continue;
+
+                var labels = AssetDatabase.GetLabels(asset);
+                labels = labels.Concat(new string[] { readonlyLabel }).ToArray();
+                AssetDatabase.SetLabels(asset, labels);
+            }
         }
 
         /// <summary>
@@ -24,25 +38,95 @@ namespace VitDeck.AssetGuardian
         /// <param name="path"></param>
         public static void Unregister(string path)
         {
-            guardPaths.Remove(path);
+            var assets = EnumerateAssets(path);
+            foreach (var asset in assets)
+            {
+                if (!IsProtected(asset))
+                    continue;
+
+                var labels = AssetDatabase.GetLabels(asset);
+                labels = labels.Where(label => label != readonlyLabel).ToArray();
+                AssetDatabase.SetLabels(asset, labels);
+            }
         }
 
         /// <summary>
         /// パスが保護対象に含まれているかどうか判定する。
         /// </summary>
-        /// <param name="assetPath">対象のパス</param>
+        /// <param name="assetPath">判定するアセットのパス</param>
         /// <returns>保護対象であればtrue、そうでなければfalse。</returns>
         public static bool Contains(string assetPath)
         {
-            foreach (var guardPath in guardPaths)
+            var asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
+            return IsProtected(asset);
+        }
+
+        private static bool IsProtected(UnityEngine.Object asset)
+        {
+            var labels = AssetDatabase.GetLabels(asset);
+
+            foreach (var label in labels)
             {
-                if (assetPath.StartsWith(guardPath))
-                {
+                if (label == readonlyLabel)
                     return true;
-                }
             }
 
             return false;
+        }
+
+        /// <summary>
+        /// 指定されたファイル/ディレクトリ及び、その子のパスを列挙する。
+        /// </summary>
+        /// <param name="path">ファイルもしくはディレクトリのパス</param>
+        /// <returns>パスの列挙</returns>
+        private static IEnumerable<string> EnumerateFileSystemEntries(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                yield return path;
+
+                var childFiles = Directory.GetFiles(path);
+                foreach (var childFile in childFiles)
+                {
+                    yield return childFile;
+                }
+
+                var childDirectories = Directory.GetDirectories(path);
+                foreach (var childDirectory in childDirectories)
+                {
+                    var progenies = EnumerateFileSystemEntries(childDirectory);
+                    foreach (var progeny in progenies)
+                        yield return progeny;
+                }
+            }
+            else if (File.Exists(path))
+            {
+                yield return path;
+                yield break;
+            }
+            else yield break;
+        }
+
+        /// <summary>
+        /// 指定されたファイル/ディレクトリ及び、あればその子のアセットを列挙する。
+        /// </summary>
+        /// <param name="path">アセットもしくはディレクトリのパス</param>
+        /// <returns>アセットの列挙</returns>
+        private static IEnumerable<UnityEngine.Object> EnumerateAssets(string path)
+        {
+            foreach (var assetPath in EnumerateFileSystemEntries(path))
+            {
+                if (assetPath.EndsWith(".meta"))
+                {
+                    continue;
+                }
+                var asset = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(assetPath);
+                if (asset == null)
+                {
+                    continue;
+                }
+                yield return asset;
+            }
         }
     }
 }
