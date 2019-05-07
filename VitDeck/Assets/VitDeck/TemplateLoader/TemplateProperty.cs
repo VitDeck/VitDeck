3464130a30d46ab4c8c6af32513856c233abc4e6
Kozu <kozu.vr@gmail.com>
2019-05-07 14:11:14 +0900

    Update TemplateProperty.cs
    Set default file name.

--- VitDeck/Assets/VitDeck/TemplateLoader/TemplateProperty.cs
+++ VitDeck/Assets/VitDeck/TemplateLoader/TemplateProperty.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "VitDeck/Template Property")]
+[CreateAssetMenu(fileName = "Property.asset", menuName = "VitDeck/Template Property")]
 public class TemplateProperty : ScriptableObject
 {
     [SerializeField]
