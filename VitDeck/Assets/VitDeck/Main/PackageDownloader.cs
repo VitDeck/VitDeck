0c3aeaedbb5a261e4f5fe54a42ae1e6e77a96437
sunafukin <ookaki7@gmail.com>
2019-05-18 16:52:40 +0900

    Add: ダウンロード機能

---
+++ VitDeck/Assets/VitDeck/Main/PackageDownloader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.IO;
+using UnityEditor;
+
+namespace VitDeck.Main
+{
+   	/// <summary>
+	/// unitypackageのダウンロードを行うクラス。
+	/// </summary>
+    public class PackageDownloader
+    {
+        public float loading = 0.0f;
+        
+        public void Download(string url, string package_name)
+        {
+            var downloader = DownloadCoroutine(url, package_name);
+            while (downloader.MoveNext()) { }
+            
+            Debug.Log("Download is complete.");
+        }
+        
+        IEnumerator DownloadCoroutine(string url, string package_name)
+        {
+            var request = UnityWebRequest.Get(url);
+            request.SendWebRequest();
+
+            while (!request.isDone)
+            {
+                loading = request.downloadProgress;
+                Debug.Log("Download: " + loading + "%");            
+                yield return null;
+            }
+
+            if (request.isHttpError || request.isNetworkError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                File.WriteAllBytes(Application.dataPath + "/" + package_name,
+                    request.downloadHandler.data);
+            }
+        }
+    }
+
+}
