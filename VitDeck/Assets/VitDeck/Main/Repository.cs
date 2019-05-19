3e0be6f4096c6f9ac7dd7fced41a9438be9cda61
sunafukin <ookaki7@gmail.com>
2019-05-19 20:42:53 +0900

    Add: VitDeckのGitHubリポジトリ関連情報の管理クラス

---
+++ VitDeck/Assets/VitDeck/Main/Repository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VitDeck.Main
+{
+    /// <summary>
+    /// VitDeckのGitHubリポジトリ関連の情報管理クラス。
+    /// </summary>
+    public class Repository
+    {
+        private static readonly string owner = "vkettools";
+        private static readonly string repository = "VitDeck";
+        private static readonly string github_url = "https://github.com";
+        private static readonly string github_api = "https://api.github.com/repos/";
+               
+        public static string GetLatestReleaseURL()
+        {
+            return github_api + owner + repository + "/releases/latest";
+        }
+
+        public string GetPackageName(string tag)
+        {
+            return "VitDeck-" + tag + ".unitypackage";
+        }
+
+        public string GetDownloadURL(string tag)
+        {
+            return String.Format("{0}/{1}/{2]/{3}/{4}/{5}",
+                            github_url,
+                            owner,
+                            repository,
+                            "releases/download",
+                            tag,
+                            GetPackageName(tag));
+        }
+    }
+}
