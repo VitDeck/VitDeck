2ae0d16290bb51efa851bfe91e49c8d20bf73216
sunafukin <ookaki7@gmail.com>
2019-05-19 19:15:04 +0900

    Add: タグ名チェックのテスト

--- VitDeck/Assets/VitDeck/Main/Tests/UpdateCheckTest.cs
+++ VitDeck/Assets/VitDeck/Main/Tests/UpdateCheckTest.cs
@@ -5,12 +5,14 @@ namespace VitDeck.Main.Tests
 {
     public class UpdateCheckTest
     {
+        private static readonly string test_url = "https://vkettools.github.io/VitDeckTest/releases/latest.json";
+        
         [Test]
-        public void TestVitDeckVersion()
+        public void TestLatestVersioning()
         {
-            var check = new UpdateCheck();
-            string version = check.GetLatestVersion();
-            Assert.That(VersionUtility.IsSemanticVersioning(version), Is.True);
+            string tag = UpdateCheck.GetLatestVersion(test_url);
+            Assert.That(VersionUtility.IsSemanticVersioning(tag), Is.True);
+            Assert.That(tag == "0.0.0");
         }
     }
 }
