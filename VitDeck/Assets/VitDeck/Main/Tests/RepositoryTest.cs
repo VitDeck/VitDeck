1bcea0c94e985f72dd59301015953dc35c9d7356
sunafukin <ookaki7@gmail.com>
2019-05-24 01:09:20 +0900

    Add: GitHubリポジトリ関連情報クラスのテストコード作成

---
+++ VitDeck/Assets/VitDeck/Main/Tests/RepositoryTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace VitDeck.Main.Tests
+{
+    public class RepositoryTest
+    {
+        [Test]
+        public void TestLatestReleaseURL()
+        {
+            string testURL = "https://api.github.com/repos/vkettools/VitDeck/releases/latest";
+            string latestReleaseURL = Repository.GetLatestReleaseURL();
+            Assert.AreEqual(latestReleaseURL, testURL);
+        }
+
+        [Test]
+        public void TestGetPackageName()
+        {
+            Assert.AreEqual(Repository.GetPackageName("0.0.1"), "VitDeck-0.0.1.unitypackage");
+            Assert.AreEqual(Repository.GetPackageName("0.1.0"), "VitDeck-0.1.0.unitypackage");
+            Assert.AreEqual(Repository.GetPackageName("1.0.0"), "VitDeck-1.0.0.unitypackage");
+            Assert.That(Repository.GetPackageName("1.0"), Is.Null);
+            Assert.That(Repository.GetPackageName("1.0.0.0"), Is.Null);
+            Assert.That(Repository.GetPackageName("１．０．０"), Is.Null);
+            Assert.That(Repository.GetPackageName("1.０.0"), Is.Null);
+            Assert.That(Repository.GetPackageName("v1.0.0"), Is.Null);
+            Assert.That(Repository.GetPackageName("ver1.0.0"), Is.Null);
+        }
+
+        [Test]
+        public void TestDownloadURL()
+        {
+            Assert.AreEqual(Repository.GetDownloadURL("0.0.1"),
+                "https://github.com/vkettools/VitDeck/releases/download/0.0.1/VitDeck-0.0.1.unitypackage");
+            Assert.AreEqual(Repository.GetDownloadURL("0.1.0"),
+                "https://github.com/vkettools/VitDeck/releases/download/0.1.0/VitDeck-0.1.0.unitypackage");
+            Assert.AreEqual(Repository.GetDownloadURL("1.0.0"),
+                "https://github.com/vkettools/VitDeck/releases/download/1.0.0/VitDeck-1.0.0.unitypackage");
+            Assert.That(Repository.GetDownloadURL("1.0"), Is.Null);
+            Assert.That(Repository.GetDownloadURL("1.0.0.0"), Is.Null);
+            Assert.That(Repository.GetDownloadURL("１．０．０"), Is.Null);
+            Assert.That(Repository.GetDownloadURL("1.０.0"), Is.Null);
+            Assert.That(Repository.GetDownloadURL("v1.0.0"), Is.Null);
+            Assert.That(Repository.GetDownloadURL("ver1.0.0"), Is.Null);
+        }
+    }
+}
