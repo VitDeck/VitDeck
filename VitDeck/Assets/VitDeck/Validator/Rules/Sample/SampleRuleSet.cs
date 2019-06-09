28cf5c99fea33a353837d24d008ff1afe66a3174
Kozu <kozu.vr@gmail.com>
2019-06-09 16:22:52 +0900

    Fix test

--- VitDeck/Assets/VitDeck/Validator/Rules/Sample/SampleRuleSet.cs
+++ VitDeck/Assets/VitDeck/Validator/Rules/Sample/SampleRuleSet.cs
@@ -9,11 +9,11 @@ namespace VitDeck.Validator
         {
             get { return "サンプルルールセット"; }
         }
-        [Validation(order = -2)]
-        public IRule unityVersionRule = new UnityVersionRule("Unityバージョンルール", "2017.4.15f1");
         [Validation(order = -1)]
         public IRule rule1 = new SampleRule("ルール１");
         [Validation]
         public IRule rule2 = new SampleRule("ルール２", "カスタム設定値２");
+        [Validation]
+        public IRule unityVersionRule = new UnityVersionRule("Unityバージョンルール", "2017.4.15f1");
     }
 }
