1935bbfb3f0146441893c388e69ca4aa8a811fd8
Kozu <kozu.vr@gmail.com>
2019-06-11 16:22:07 +0900

    Add ValidationTargetFinder Test

---
+++ VitDeck/Assets/VitDeck/Validator/Tests/TestValidationTargetFinder.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+using UnityEditor;
+
+namespace VitDeck.Validator.Test
+{
+    public class TestValidationTargetFinder
+    {
+        [Test]
+        public void TestFind()
+        {
+            var testFolder = "Assets/VitDeck/Validator/Tests/ValidationTargetFinder";
+            var finder = new ValidationTargetFinder();
+            var validationTarget = finder.Find(testFolder);
+            Assert.That(validationTarget.GetBaseFolderPath(), Is.EqualTo(testFolder));
+            Assert.That(validationTarget.GetAllAssetPaths(), Is.Not.Null);
+            Assert.That(validationTarget.GetAllAssetGuids(), Is.Not.Null);
+            Assert.That(validationTarget.GetAllAssets(), Is.Not.Null);
+            //Assert.That(validationTarget.GetScenes(), Is.Not.Null); //ToDo:実装
+            //Assert.That(validationTarget.GetRootObjects(), Is.Not.Null); //ToDo:実装
+            //Assert.That(validationTarget.GetAllObjects(), Is.Not.Null); //ToDo:実装
+        }
+        [Test]
+        public void TestFindAssetPaths()
+        {
+            var testFolder = "Assets/VitDeck/Validator/Tests/ValidationTargetFinder";
+            var finder = new ValidationTargetFinder();
+            var assetPaths = finder.FindAssetPaths(testFolder);
+            Assert.That(Array.Exists(assetPaths, path => path == (testFolder + "/Sample_object.fbx")), Is.True);
+            Assert.That(Array.Exists(assetPaths, path => path == (testFolder + "/New Scene.unity")), Is.True);
+            Assert.That(Array.Exists(assetPaths, path => path == (testFolder + "/Sub Fonder")), Is.True);
+            Assert.That(Array.Exists(assetPaths, path => path == (testFolder + "/Sub Fonder/Scene in sub folder.unity")), Is.True);
+            //FBXのサブアセットによりパスが重複取得されない
+            Assert.That(assetPaths.Length, Is.EqualTo(assetPaths.Distinct().Count()));
+        }
+        [Test]
+        public void TestFindAssetGuids()
+        {
+            var testFolder = "Assets/VitDeck/Validator/Tests/ValidationTargetFinder";
+            var finder = new ValidationTargetFinder();
+            var assetGuids = finder.FindAssetGuids(testFolder);
+            //New Scene.unity
+            Assert.That(Array.Exists(assetGuids, guid => guid == "f1f69f0382468de4284c5b19f5d367e5"), Is.True);
+            //Sample_object.fbx 
+            Assert.That(Array.Exists(assetGuids, guid => guid == "a4b03051a833cca449d86b3821e3079f"), Is.True);
+            //Sub Fonder
+            Assert.That(Array.Exists(assetGuids, guid => guid == "de218765854f6bf4ba69502dfe9b1e86"), Is.True);
+            //Scene in sub folder.unity
+            Assert.That(Array.Exists(assetGuids, guid => guid == "068dc3056368a43488efe92a2c772309"), Is.True);
+            //FBXのサブアセットによりGUIDが重複取得されない
+            Assert.That(assetGuids.Length, Is.EqualTo(assetGuids.Distinct().Count()));
+        }
+        [Test]
+        public void TestFindAssetObjects()
+        {
+            var testFolder = "Assets/VitDeck/Validator/Tests/ValidationTargetFinder";
+            var finder = new ValidationTargetFinder();
+            var assetObjects = finder.FindAssetObjects(testFolder);
+            //New Scene.unity
+            Assert.That(Array.Exists(assetObjects, obj => AssetDatabase.GetAssetPath(obj) == (testFolder + "/New Scene.unity")), Is.True);
+            //Sample_object.fbx 
+            Assert.That(Array.Exists(assetObjects, obj => AssetDatabase.GetAssetPath(obj) == (testFolder + "/Sample_object.fbx")), Is.True);
+            //Sub Fonder
+            Assert.That(Array.Exists(assetObjects, obj => AssetDatabase.GetAssetPath(obj) == (testFolder + "/Sub Fonder")), Is.True);
+            //Scene in sub folder.unity
+            Assert.That(Array.Exists(assetObjects, obj => AssetDatabase.GetAssetPath(obj) == (testFolder + "/Sub Fonder/Scene in sub folder.unity")), Is.True);
+            //FBXのサブアセットによりオブジェクトが重複取得されない
+            Assert.That(assetObjects.Length, Is.EqualTo(assetObjects.Distinct().Count()));
+        }
+        [Test]
+        public void TestInvalidPath()
+        {
+            var finder = new ValidationTargetFinder();
+            var path = "invalidPath";
+            Assert.That(finder.FindAssetPaths(path), Is.Null);
+            Assert.That(finder.FindAssetGuids(path), Is.Null);
+            Assert.That(finder.FindAssetObjects(path), Is.Null);
+            Assert.That(finder.FindScenes(path), Is.Null);
+            Assert.That(finder.FindRootObjects(path), Is.Null);
+            Assert.That(finder.FindAllObjects(path), Is.Null);
+        }
+    }
+}
--- VitDeck/Assets/VitDeck/Validator/Tests/TestValidator.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/TestValidator.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using System.Text.RegularExpressions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace VitDeck.Validator.Test
@@ -38,6 +39,55 @@ namespace VitDeck.Validator.Test
             var ruleSet = new SampleRuleSet();
             Assert.That(ruleSet.RuleSetName, Is.EqualTo("サンプルルールセット"));
             Assert.That(ruleSet.GetRules().Length, Is.AtLeast(1));
+        }
+        [Test]
+        public void TestValidationtarget()
+        {
+            var baseFolderPath = "Assets/VitDeck/Validator/Tests";
+            var assetGuids = new string[] { "testguid" };
+            var assetPaths = new string[] { "testPath", "testPath2" };
+            var assetObjects = new Object[] { };
+            var scenes = new Scene[] { new Scene() };
+            var rootObjects = new GameObject[] { };
+            var allObjects = new GameObject[] { };
+            var target = new ValidationTarget(baseFolderPath,
+                assetGuids,
+                assetPaths,
+                assetObjects,
+                scenes,
+                rootObjects,
+                allObjects);
+            Assert.That(target.GetBaseFolderPath(), Is.EqualTo(baseFolderPath));
+            Assert.That(target.GetAllAssetGuids(), Is.EqualTo(assetGuids));
+            Assert.That(target.GetAllAssetPaths(), Is.EqualTo(assetPaths));
+            Assert.That(target.GetAllAssets(), Is.EqualTo(assetObjects));
+            Assert.That(target.GetScenes(), Is.EqualTo(scenes));
+            Assert.That(target.GetRootObjects(), Is.EqualTo(rootObjects));
+            Assert.That(target.GetAllObjects(), Is.EqualTo(allObjects));
+
+        }
+        [Test]
+        public void TestValidationtargetError()
+        {
+            var target = new ValidationTarget("Assets/VitDeck/Validator/Tests");
+            Assert.That(() => target.GetAllAssetGuids(),
+                Throws.Exception.TypeOf<FatalValidationErrorException>()
+                .And.Message.EqualTo("Faild to get asset GUIDs."));
+            Assert.That(() => target.GetAllAssetPaths(),
+                Throws.Exception.TypeOf<FatalValidationErrorException>()
+                .And.Message.EqualTo("Faild to get asset Paths."));
+            Assert.That(() => target.GetAllAssets(),
+                Throws.Exception.TypeOf<FatalValidationErrorException>()
+                .And.Message.EqualTo("Faild to get asset Objects."));
+            Assert.That(() => target.GetScenes(),
+                Throws.Exception.TypeOf<FatalValidationErrorException>()
+                .And.Message.EqualTo("Faild to get scenes."));
+            Assert.That(() => target.GetRootObjects(),
+                 Throws.Exception.TypeOf<FatalValidationErrorException>()
+                 .And.Message.EqualTo("Faild to get root objects."));
+            Assert.That(() => target.GetAllObjects(),
+                 Throws.Exception.TypeOf<FatalValidationErrorException>()
+                 .And.Message.EqualTo("Faild to get all objects."));
         }
     }
 }
