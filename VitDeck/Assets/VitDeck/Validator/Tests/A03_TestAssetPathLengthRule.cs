7e13a7e7bb3c0e99571fdfec113a02f35d85abc9
Sokuhatiku <sokuhatiku@gmail.com>
2019-06-15 23:53:18 +0900

    add more test

--- VitDeck/Assets/VitDeck/Validator/Tests/A03_TestAssetPathLengthRule.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/A03_TestAssetPathLengthRule.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Text.RegularExpressions;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -11,10 +12,10 @@ namespace VitDeck.Validator.Test
         public void TestValidateNoTargets()
         {
             var version = UnityEngine.Application.unityVersion;
-            var rule = new AssetPathLengthRule("アセットパス長テスト");
+            var rule = new AssetPathLengthRule("アセットパス長制限テスト");
             var target = new ValidationTarget("Assets/VitDeck/Validator/Tests", assetPaths: new string[] { });
             var result = rule.Validate(target);
-            Assert.That(result.RuleName, Is.EqualTo("アセットパス長テスト"));
+            Assert.That(result.RuleName, Is.EqualTo("アセットパス長制限テスト"));
             Assert.That(result.Issues.Count, Is.EqualTo(0));
         }
         [Test]
@@ -24,15 +25,21 @@ namespace VitDeck.Validator.Test
             var targetAssetPaths = new string[] { targetAssetPath };
             var target = new ValidationTarget("Assets/VitDeck/Validator/Tests", assetPaths: targetAssetPaths);
 
-            var willPassRule = new AssetPathLengthRule("アセットパス長テスト", targetAssetPath.Length);
-            var willFailRule = new AssetPathLengthRule("アセットパス長テスト", targetAssetPath.Length - 1);
+            var willPassRule = new AssetPathLengthRule("アセットパス長制限テスト", targetAssetPath.Length);
+            var willFailRule = new AssetPathLengthRule("アセットパス長制限テスト", targetAssetPath.Length - 1);
 
             var passedResult = willPassRule.Validate(target);
             Assert.That(passedResult.Issues.Count, Is.EqualTo(0));
 
             var failedResult = willFailRule.Validate(target);
             Assert.That(failedResult.Issues.Count, Is.EqualTo(1));
-            Assert.That(failedResult.Issues[0].level, Is.EqualTo(IssueLevel.Error));
+            var issue = failedResult.Issues[0];
+            Assert.That(issue.level, Is.EqualTo(IssueLevel.Error));
+            Assert.That(issue.target, Is.EqualTo(AssetDatabase.LoadAssetAtPath<Object>(targetAssetPath)));
+            Assert.That(issue.message, Is.EqualTo(System.String.Format("長すぎるパスがあります。（制限={0}, 超過={1}, パス={2}）", targetAssetPath.Length - 1, 1, targetAssetPath)));
+            Assert.That(issue.solution, Is.Empty);
+            Assert.That(issue.solutionURL, Is.Empty);
+
         }
     }
 }
