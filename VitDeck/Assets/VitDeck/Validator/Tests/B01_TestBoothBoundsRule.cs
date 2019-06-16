d39817e6f59b163e996c1f6a541b5c0af78163be
Sokuhatiku <sokuhatiku@gmail.com>
2019-06-16 22:36:59 +0900

    Add RectTransform Test

--- VitDeck/Assets/VitDeck/Validator/Tests/B01_TestBoothBoundsRule.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/B01_TestBoothBoundsRule.cs
@@ -19,6 +19,8 @@ namespace VitDeck.Validator
                 CreateSimpleGameObject("TestTargetObject2", false, new Vector3(1000, 0, 0)),
                 CreateRendererGameObject("TestTargetObject3", true, new Vector3(0, 1, 0), new Vector3(2, 2, 2)),
                 CreateRendererGameObject("TestTargetObject4", false, new Vector3(0, 100, 0), new Vector3(200, 200, 200)),
+                CreateRectGameObject("TestTargetObject5", true, new Vector3(0, 1, 0), new Vector2(2, 2) ),
+                CreateRectGameObject("TestTargetObject6", false, new Vector3(0, 1, 0), new Vector2(2, 2) ),
             };
 
             targetContainer = new ValidationTarget("Assets/VitDeck/Validator/Tests", allObjects: targetObjects);
@@ -28,7 +30,7 @@ namespace VitDeck.Validator
         {
             CreateTestCase( "TestRule0", Vector3.zero, new Vector3(3, 5, 4), 0f, FailedTargetObjectIndices(null) ),
             CreateTestCase( "TestRule1", Vector3.zero, new Vector3(2, 5, 4), 0f, FailedTargetObjectIndices(null) ),
-            CreateTestCase( "TestRule2", Vector3.zero, new Vector3(1.9f, 5, 4), 0f, FailedTargetObjectIndices(1, 3) ),
+            CreateTestCase( "TestRule2", Vector3.zero, new Vector3(1.9f, 5, 4), 0f, FailedTargetObjectIndices(1, 3, 5) ),
             CreateTestCase( "TestRule3", Vector3.zero, new Vector3(1.9f, 5, 4), 0.1f, FailedTargetObjectIndices(null) ),
         };
 
@@ -86,10 +88,18 @@ namespace VitDeck.Validator
             var mesh = new Mesh();
             mesh.bounds = new Bounds(Vector3.zero, meshSize);
 
-            var gameObject = new GameObject(name, typeof(MeshFilter), typeof(MeshRenderer));
-            gameObject.SetActive(isActive);
-            gameObject.transform.position = position;
-            gameObject.GetComponent<MeshFilter>().mesh = mesh;
+            var gameObject = CreateSimpleGameObject(name, isActive, position);
+            gameObject.AddComponent<MeshRenderer>();
+            gameObject.AddComponent<MeshFilter>().mesh = mesh;
+
+            return gameObject;
+        }
+
+        public static GameObject CreateRectGameObject(string name, bool isActive, Vector3 position, Vector2 size)
+        {
+            var gameObject = CreateSimpleGameObject(name, isActive, position);
+            var transform = gameObject.AddComponent<RectTransform>();
+            transform.sizeDelta = size;
 
             return gameObject;
         }
