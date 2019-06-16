d7891e19d70512de9070f9c5574215baea16a108
Kozu <kozu.vr@gmail.com>
2019-06-17 00:24:15 +0900

    Update SampleRuleSet.cs
    Add new rules

--- VitDeck/Assets/VitDeck/Validator/Rules/Sample/SampleRuleSet.cs
+++ VitDeck/Assets/VitDeck/Validator/Rules/Sample/SampleRuleSet.cs
@@ -10,15 +10,19 @@ namespace VitDeck.Validator
             get { return "サンプルルールセット"; }
         }
         [Validation(order = -1)]
-        public IRule rule1 = new SampleRule("ルール１");
-        [Validation]
-        public IRule rule2 = new SampleRule("ルール２", "カスタム設定値２");
+        public IRule rule1 = new SampleRule("サンプルメッセージ");
         [Validation]
         public IRule unityVersionRule = new UnityVersionRule("[U01]Unityバージョンルール", "2017.4.15f1");
+        [Validation]
+        public IRule assetNamingRule = new AssetNamingRule("[A01]アセット名の使用禁止文字ルール", "[\x21-\x7e ]+");
         [Validation]
         public IRule assetGuidBlacklistRule = new AssetGuidBlacklistRule("[A02]特定のGUIDを持つアセットの検出ルール",
             new string[] { "740112f6e77ca914d9c26eef5d68accd", "ae68339621fb41b4f9905188526120ea" });
         [Validation]
         public IRule assetPathLengthRule = new AssetPathLengthRule("[A03]アセットパス長制限ルール", 184);
+        [Validation]
+        public IRule assetExtentionBlacklistRule = new AssetExtentionBlacklistRule("[A04]アセット拡張子ルール", new string[] { ".txt", ".md" });
+        [Validation]
+        public IRule boothBoundsRule = new BoothBoundsRule("[B01]ブースBounds超過検証ルール", new UnityEngine.Vector3(4.0f, 5.0f, 4.0f), 0.01f);
     }
 }
