10ff3f25c85307d734a967c39c172ee10569011d
sunafukin <ookaki7@gmail.com>
2019-06-23 22:10:34 +0900

    Fix: オブジェクト名の重複の削除とIssueLevelの変更

--- VitDeck/Assets/VitDeck/Validator/Tests/B04_TestErrorShaderRule.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/B04_TestErrorShaderRule.cs
@@ -17,17 +17,17 @@ namespace VitDeck.Validator.Test
             Assert.That(result.Issues.Count, Is.EqualTo(3));
             Assert.That(result.Issues[0].level, Is.EqualTo(IssueLevel.Error));
             Assert.That(result.Issues[0].target.name, Is.EqualTo("HiddenInternalErrorShaderObject"));
-            Assert.That(result.Issues[0].message, Is.EqualTo(string.Format("オブジェクト({0})のマテリアルで正しいシェーダーが参照されていません。", "HiddenInternalErrorShaderObject")));
+            Assert.That(result.Issues[0].message, Is.EqualTo(string.Format("オブジェクトのマテリアルで正しいシェーダーが参照されていません。", "HiddenInternalErrorShaderObject")));
             Assert.That(result.Issues[0].solution, Is.Empty);
             Assert.That(result.Issues[0].solutionURL, Is.Empty);
             Assert.That(result.Issues[1].level, Is.EqualTo(IssueLevel.Error));
             Assert.That(result.Issues[1].target.name, Is.EqualTo("ErrorShaderObject"));
-            Assert.That(result.Issues[1].message, Is.EqualTo(string.Format("オブジェクト({0})でシェーダーエラーが検出されました。", "ErrorShaderObject")));
+            Assert.That(result.Issues[1].message, Is.EqualTo(string.Format("オブジェクトでシェーダーエラーが検出されました。", "ErrorShaderObject")));
             Assert.That(result.Issues[1].solution, Is.Empty);
             Assert.That(result.Issues[1].solutionURL, Is.Empty);
-            Assert.That(result.Issues[2].level, Is.EqualTo(IssueLevel.Error));
+            Assert.That(result.Issues[2].level, Is.EqualTo(IssueLevel.Info));
             Assert.That(result.Issues[2].target.name, Is.EqualTo("NoneMaterialObject"));
-            Assert.That(result.Issues[2].message, Is.EqualTo(string.Format("オブジェクト({0})のRendererにマテリアルの参照がありません。", "NoneMaterialObject")));
+            Assert.That(result.Issues[2].message, Is.EqualTo(string.Format("オブジェクトのRendererにマテリアルの参照がありません。", "NoneMaterialObject")));
             Assert.That(result.Issues[2].solution, Is.Empty);
             Assert.That(result.Issues[2].solutionURL, Is.Empty);
         }
