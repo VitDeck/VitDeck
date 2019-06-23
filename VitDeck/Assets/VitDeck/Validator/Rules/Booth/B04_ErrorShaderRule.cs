8571e4c6e7dd261cb87f5d2f403d3269f788cbd3
sunafukin <ookaki7@gmail.com>
2019-06-23 19:35:39 +0900

    Add: エラーシェーダーの検出ルール追加

---
+++ VitDeck/Assets/VitDeck/Validator/Rules/Booth/B04_ErrorShaderRule.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Linq;
+
+namespace VitDeck.Validator
+{
+    /// <summary>
+    /// エラーシェーダーの検出ルール
+    /// </summary>
+    /// <remarks>シェーダーエラーの存在するオブジェクトを検出する。</remarks>
+    public class ErrorShaderRule : BaseRule
+    {
+        /// <summary>
+        /// コンストラクタ。
+        /// </summary>
+        /// <param name="name">ルール名</param>
+        public ErrorShaderRule(string name) : base(name) { }
+
+        protected override void Logic(ValidationTarget target)
+        {
+            foreach (var obj in target.GetAllObjects())
+            {
+                if (obj.GetComponent<Renderer>() == null)
+                    continue;
+
+                var materials = obj.GetComponent<Renderer>().sharedMaterials;
+
+                foreach (var material in materials)
+                {
+                    if (material == null)
+                    {
+                        continue;
+                    }
+
+                    if (material.shader.name == "Hidden/InternalErrorShader" || material.shader.name == string.Empty || material.shader.name == null)
+                    {
+                        AddIssue(new Issue(obj, IssueLevel.Error, string.Format("シェーダエラーが存在するオブジェクト({0})があります。", obj.name), string.Empty, string.Empty));
+                    }
+                }
+            }
+        }
+    }
+}
