d929b769dd56036b87c8191d25dab2f2bff6e27d
Sokuhatiku <sokuhatiku@gmail.com>
2019-12-13 02:00:04 +0900

    VRCSDKVersionRuleを作成

---
+++ VitDeck/Assets/VitDeck/Validator/Rules/Vket4/A02_VRCSDKVersionRule.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace VitDeck.Validator
+{
+    public class A02_VRCSDKVersionRule : BaseRule
+    {
+        const string versionFilePath = "Assets/VRCSDK/version.txt";
+
+        const string sdkDownloadURL = "https://www.vrchat.net/download/sdk";
+
+        private VRCSDKVersion targetVersion;
+
+        public A02_VRCSDKVersionRule(string name, VRCSDKVersion version) : base(name)
+        {
+            targetVersion = version;
+        }
+
+        protected override void Logic(ValidationTarget target)
+        {
+
+            if (!File.Exists(versionFilePath))
+            {
+                AddIssue(new Issue(null,
+                    IssueLevel.Error,
+                    "VRCSDKがインポートされていません。",
+                    "公式サイトからダウンロードして下さい。",
+                    solutionURL: "https://www.vrchat.net/download/sdk"
+                    ));
+                return;
+            }
+
+            var currentVersion = new VRCSDKVersion(File.ReadAllText(versionFilePath));
+
+            if (currentVersion < targetVersion)
+            {
+                AddIssue(new Issue(null,
+                    IssueLevel.Error,
+                    "VRCSDKが最新バージョンではありません。",
+                    "公式サイトからダウンロードして下さい。",
+                    solutionURL: "https://www.vrchat.net/download/sdk"
+                    ));
+            }
+        }
+    }
+}
---
+++ VitDeck/Assets/VitDeck/Validator/Tests/VRCSDKVersionTest.cs
@@ -0,0 +1,61 @@
+using NUnit.Framework;
+
+namespace VitDeck.Validator
+{
+    [TestFixture(TestOf = typeof(A02_VRCSDKVersionRule))]
+    public class VRCSDKVersionTest
+    {
+        [Test]
+        public void OperatorTest()
+        {
+            var version = new VRCSDKVersion("2019.09.18.12.05");
+            var sameVersion = new VRCSDKVersion("2019.09.18.12.05");
+            var prevVersion = new VRCSDKVersion("2019.09.18.12.04");
+            var nextVersion = new VRCSDKVersion("2019.09.18.12.06");
+
+            Assert.IsTrue(version > prevVersion);
+            Assert.IsFalse(version > sameVersion);
+            Assert.IsFalse(version > nextVersion);
+
+            Assert.IsFalse(version < prevVersion);
+            Assert.IsFalse(version < sameVersion);
+            Assert.IsTrue(version < nextVersion);
+
+            Assert.IsTrue(version >= prevVersion);
+            Assert.IsTrue(version >= sameVersion);
+            Assert.IsFalse(version >= nextVersion);
+
+            Assert.IsFalse(version <= prevVersion);
+            Assert.IsTrue(version <= sameVersion);
+            Assert.IsTrue(version <= nextVersion);
+
+            Assert.IsFalse(version == prevVersion);
+            Assert.IsTrue(version == sameVersion);
+            Assert.IsFalse(version == nextVersion);
+
+            Assert.IsTrue(version != prevVersion);
+            Assert.IsFalse(version != sameVersion);
+            Assert.IsTrue(version != nextVersion);
+        }
+
+        [Test]
+        public void InterconvertibleTest()
+        {
+            var version = new VRCSDKVersion("2019.09.18.12.05");
+
+            var regeneratedVersion = new VRCSDKVersion(version.ToInterconvertibleString());
+
+            Assert.AreEqual(version, regeneratedVersion);
+        }
+
+        [Test]
+        public void InvalidVersionTest()
+        {
+            Assert.Throws<InvalidVersionFormatException>(() => new VRCSDKVersion(""));
+            Assert.Throws<InvalidVersionFormatException>(() => new VRCSDKVersion("2019.09.18.12.05.01"));
+            Assert.Throws<InvalidVersionFormatException>(() => new VRCSDKVersion("2019.09.18.12"));
+            Assert.Throws<InvalidVersionFormatException>(() => new VRCSDKVersion("2019.09.18.12.abc"));
+
+        }
+    }
+}
