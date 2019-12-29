789a4afdab620a6e302d79749e09e9170af661ac
Sokuhatiku <sokuhatiku@gmail.com>
2019-12-29 23:09:52 +0900

    ResetTokenSourceのテストを追加

---
+++ VitDeck/Assets/VitDeck/Validator/Tests/TestResetTokenSource.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+using VitDeck.Validator.BoundsIndicators;
+
+namespace VitDeck.Validator.Test
+{
+    [TestFixture(TestOf = typeof(ResetTokenSource))]
+    public class TestResetTokenSource
+    {
+        [Test]
+        private void Test()
+        {
+            var tokenSource = new ResetTokenSource();
+
+            var token = tokenSource.Token;
+
+            bool isCalled = false;
+
+            token.Reset += () => isCalled = true;
+
+            tokenSource.Reset();
+
+            Assert.IsTrue(isCalled);
+        }
+    }
+}
