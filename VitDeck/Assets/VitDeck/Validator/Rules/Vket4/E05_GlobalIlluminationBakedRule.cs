dc53bf528dc41d96621cc0f0f31ada626f667996
sunafukin <ookaki7@gmail.com>
2020-01-12 23:11:08 +0900

    [E05]StandardShaderを使うMaterialがEmissionを使用している場合、Global IlluminationがBakedであることをチェックするルールを追加

---
+++ VitDeck/Assets/VitDeck/Validator/Rules/Vket4/E05_GlobalIlluminationBakedRule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using System.Linq;
+
+namespace VitDeck.Validator
+{
+	public class E05_GlobalIlluminationBakedRule : BaseRule
+	{
+		public E05_GlobalIlluminationBakedRule(string name) : base(name)
+		{
+		}
+
+		protected override void Logic(ValidationTarget target)
+		{
+			var allObjects = target.GetAllObjects();
+			foreach (var referenceObject in allObjects)
+			{
+				var isNotBaked = referenceObject
+					.GetComponents<Renderer>()
+					.Select(x => x.material)
+					.Where(x => x.shader.name == "Standard")
+					.Where(x => x.IsKeywordEnabled("_EMISSION"))
+					.Any(x => x.globalIlluminationFlags != MaterialGlobalIlluminationFlags.BakedEmissive);
+				
+				if (isNotBaked)
+				{
+					var assetPath = AssetDatabase.GetAssetPath(referenceObject);
+                    var message = String.Format("アセット{0}でGlobal IlluminationがBakedに設定されていません。", assetPath);
+                    var solution = String.Format("Standard ShaderでEmissionを利用する場合はGlobal Illuminationの設定をBakedにしてください。");
+                    AddIssue(new Issue(referenceObject, IssueLevel.Error, message, solution));
+				}		
+			}
+		}
+	}
+}
