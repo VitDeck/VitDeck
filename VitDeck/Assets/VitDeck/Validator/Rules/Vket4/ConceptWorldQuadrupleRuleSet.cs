dd742cacbe753d14e72837897d68eacb74e506df
Sokuhatiku <sokuhatiku@gmail.com>
2020-01-14 23:44:46 +0900

    合同ブース用ののルールセットを作成

---
+++ VitDeck/Assets/VitDeck/Validator/Rules/Vket4/ConceptWorldQuadrupleRuleSet.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+namespace VitDeck.Validator
+{
+    public class ConceptWorldQuadrupleRuleSet : ConceptWorldRuleSet
+    {
+        public override string RuleSetName
+        {
+            get
+            {
+                return "Vket4 - ConceptWorld(Quadruple)";
+            }
+        }
+
+        protected override Vector3 BoothSizeLimit
+        {
+            get
+            {
+                return new Vector3(16, 5, 4);
+            }
+        }
+
+        protected override int MaterialUsesLimit
+        {
+            get
+            {
+                return base.MaterialUsesLimit * 4;
+            }
+        }
+
+        protected override int LightmapCountLimit
+        {
+            get
+            {
+                return base.LightmapCountLimit * 4;
+            }
+        }
+        protected override int AreaLightUsesLimit
+        {
+            get
+            {
+                return base.AreaLightUsesLimit * 4;
+            }
+        }
+
+        protected override int ChairPrefabUsesLimit
+        {
+            get
+            {
+                return base.ChairPrefabUsesLimit * 4;
+            }
+        }
+
+        protected override int PickupObjectSyncUsesLimit
+        {
+            get
+            {
+                return base.PickupObjectSyncUsesLimit * 4;
+            }
+        }
+    }
+}
