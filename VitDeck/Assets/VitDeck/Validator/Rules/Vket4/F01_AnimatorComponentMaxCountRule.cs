5d1c623e222df398d07b3b6407c255184bfc180b
sunafukin <ookaki7@gmail.com>
2020-01-16 02:37:41 +0900

    Animatorの使用数制限ルールおよびテストを追加

---
+++ VitDeck/Assets/VitDeck/Validator/Rules/Vket4/F01_AnimatorComponentMaxCountRule.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace VitDeck.Validator
+{
+	public class F01_AnimatorComponentMaxCountRule : BaseRule
+	{
+		private int maxCount;
+
+		public F01_AnimatorComponentMaxCountRule(string name, int maxCount = 50) : base(name)
+		{
+			this.maxCount = maxCount;
+		}
+
+		protected override void Logic(ValidationTarget target)
+		{
+			var allObjects = target.GetAllObjects();
+			var animatorCount = allObjects
+				.Where(x => x.GetComponent<Animator>() != null)
+				.Count();
+
+			if (animatorCount > maxCount)
+			{
+				var message = string.Format("Animatorコンポーネントの使用数が{0}個を超えています。", maxCount);
+				AddIssue(new Issue(null, IssueLevel.Error, message, string.Empty));
+			}
+		}
+	}
+}
---
+++ VitDeck/Assets/VitDeck/Validator/Tests/F01_TestAnimatorComponentMaxCountRule.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+
+namespace VitDeck.Validator.Test
+{
+    public class TestAnimatorComponentMaxCountRule
+    {
+		private readonly int ANIMATOR_MAX_COUNT = 50;
+        [Test]
+        public void TestValidate()
+        {
+            var finder = new ValidationTargetFinder();
+            var target = finder.Find("Assets/VitDeck/Validator/Tests/Data/F01_AnimatorComponentMaxCountRule", true);
+            var rule = new F01_AnimatorComponentMaxCountRule("AnimatorComponent使用数検出", ANIMATOR_MAX_COUNT);
+            var result = rule.Validate(target);
+            Assert.That(result.RuleName, Is.EqualTo("AnimatorComponent使用数検出"));
+            Assert.That(result.Issues.Count, Is.EqualTo(1));
+            Assert.That(result.Issues[0].level, Is.EqualTo(IssueLevel.Error));
+            Assert.That(result.Issues[0].target, Is.EqualTo(null));
+            Assert.That(result.Issues[0].message, Is.EqualTo(string.Format("Animatorコンポーネントの使用数が{0}個を超えています。", ANIMATOR_MAX_COUNT)));
+            Assert.That(result.Issues[0].solution, Is.Empty);
+            Assert.That(result.Issues[0].solutionURL, Is.Empty);
+        }
+    }
+}
