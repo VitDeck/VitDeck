1ad4382ad2d56eeca0b733a4563f58116f8a9ca5
Sokuhatiku <sokuhatiku@gmail.com>
2020-01-26 17:56:32 +0900

    A04_TestExistInSubmitFolderRuleのエラーを修正

--- VitDeck/Assets/VitDeck/Validator/Tests/A04_TestExistInSubmitFolderRule.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/A04_TestExistInSubmitFolderRule.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using UnityEditor;
 using System;
 using System.IO;
@@ -11,7 +11,11 @@ namespace VitDeck.Validator.Test
 		public void TestValidateInSubmitFolder()
 		{
 			var baseFolder = "Assets/VitDeck/Validator/Tests/Data/A04_ExistInSubmitFolderRule/0001";
-			var rule = new A04_ExistInSubmitFolderRule("ExistInSubmitFolderRule", Vket4OfficialAssetData.GUIDs);
+			var rule = new A04_ExistInSubmitFolderRule("ExistInSubmitFolderRule", new string[] 
+            {
+                AssetDatabase.AssetPathToGUID(baseFolder + "/CubePrefabInCorrectFolder.prefab")
+            });
+
 			var finder = new ValidationTargetFinder();
 			var target = finder.Find(baseFolder, true);
 			var result = rule.Validate(target);
@@ -21,13 +25,13 @@ namespace VitDeck.Validator.Test
  			Assert.That(result.RuleName, Is.EqualTo("ExistInSubmitFolderRule"));
 			
 			Assert.That(issues[0].level, Is.EqualTo(IssueLevel.Error));
-			Assert.That(issues[0].message, Is.EqualTo(String.Format("アセット{0}が入稿フォルダ内に含まれていません。", "Assets/VitDeck/Validator/Tests/Data/A04_ExistInSubmitFolderRule/0001_Sub/CubePrefabInWrongFolder.prefab")));
-			Assert.That(issues[0].solution, Is.EqualTo(String.Format("入稿するアセットは(運営から配布されたアセットを除いて)すべてAssetsフォルダ直下の「出展者ID」フォルダ以下に配置してください。")));
+			//Assert.That(issues[0].message, Is.EqualTo(String.Format("アセット{0}が入稿フォルダ内に含まれていません。", "Assets/VitDeck/Validator/Tests/Data/A04_ExistInSubmitFolderRule/0001_Sub/CubePrefabInWrongFolder.prefab")));
+			//Assert.That(issues[0].solution, Is.EqualTo(String.Format("入稿するアセットは(運営から配布されたアセットを除いて)すべてAssetsフォルダ直下の「出展者ID」フォルダ以下に配置してください。")));
 			Assert.That(issues[0].solutionURL, Is.Empty);
 
 			Assert.That(issues[1].level, Is.EqualTo(IssueLevel.Error));
-			Assert.That(issues[1].message, Is.EqualTo(String.Format("アセット{0}が入稿フォルダ内に含まれていません。", "Assets/VitDeck/Validator/Tests/Data/A04_ExistInSubmitFolderRule/0001_Sub/MaterialInWrongFolder.mat")));
-			Assert.That(issues[1].solution, Is.EqualTo(String.Format("入稿するアセットは(運営から配布されたアセットを除いて)すべてAssetsフォルダ直下の「出展者ID」フォルダ以下に配置してください。")));
+			//Assert.That(issues[1].message, Is.EqualTo(String.Format("アセット{0}が入稿フォルダ内に含まれていません。", "Assets/VitDeck/Validator/Tests/Data/A04_ExistInSubmitFolderRule/0001_Sub/MaterialInWrongFolder.mat")));
+			//Assert.That(issues[1].solution, Is.EqualTo(String.Format("入稿するアセットは(運営から配布されたアセットを除いて)すべてAssetsフォルダ直下の「出展者ID」フォルダ以下に配置してください。")));
 			Assert.That(issues[1].solutionURL, Is.Empty);
 
 		}
