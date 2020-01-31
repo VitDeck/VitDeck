473b966840c916a35f3dcc37518642cd02934d45
Sokuhatiku <sokuhatiku@gmail.com>
2020-02-01 04:21:46 +0900

    メッシュの子の保護設定が2018.3で正しくRestoreされていないのを修正

--- VitDeck/Assets/VitDeck/AssetGuardian/AssetRestorer.cs
+++ VitDeck/Assets/VitDeck/AssetGuardian/AssetRestorer.cs
@@ -46,13 +46,13 @@ namespace VitDeck.AssetGuardian
                 new SimpleRestorer(HideFlags.NotEditable));
             restoreTools.Add(
                 new AssetTypeIdentifier(typeof(GameObject), true),
-                new PrefabRestorer(HideFlags.None));
+                new PrefabRestorer(HideFlags.None, false));
             restoreTools.Add(
                 new AssetTypeIdentifier(typeof(AudioClip)),
                 new SimpleRestorer(HideFlags.NotEditable));
             restoreTools.Add(
                 new AssetTypeIdentifier(typeof(GameObject), false),
-                new PrefabRestorer(HideFlags.NotEditable));
+                new PrefabRestorer(HideFlags.NotEditable, true));
             restoreTools.Add(
                 new AssetTypeIdentifier(typeof(Shader)),
                 new SimpleRestorer(HideFlags.NotEditable));
@@ -87,9 +87,11 @@ namespace VitDeck.AssetGuardian
         private class PrefabRestorer : IRestorer
         {
             private readonly HideFlags baseHideFlags;
-            public PrefabRestorer(HideFlags baseHideFlags)
+            private bool isModel;
+            public PrefabRestorer(HideFlags baseHideFlags, bool isModel)
             {
                 this.baseHideFlags = baseHideFlags;
+                this.isModel = isModel;
             }
 
             public void Restore(Object asset)
@@ -114,7 +116,14 @@ namespace VitDeck.AssetGuardian
             private static bool IsShownInProjectView(Transform rootTransform, Transform transform)
             {
 #if UNITY_2018_3_OR_NEWER
-                return transform == rootTransform;
+                if (isModel)
+                {
+                    return transform == rootTransform || transform.parent == rootTransform;
+                }
+                else
+                {
+                    return transform == rootTransform;
+                }
 #else
                 return transform == rootTransform || transform.parent == rootTransform;
 #endif
