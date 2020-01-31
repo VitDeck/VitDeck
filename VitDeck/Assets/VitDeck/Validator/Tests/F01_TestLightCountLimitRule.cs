e36e2846432ff877f8d8c8c19150f75e75451ca3
Sokuhatiku <sokuhatiku@gmail.com>
2020-02-01 04:34:04 +0900

    2018.3でLightTypeのエイリアスが出来たことによりテストが落ちるようになっていたのを修正

--- VitDeck/Assets/VitDeck/Validator/Tests/F01_TestLightCountLimitRule.cs
+++ VitDeck/Assets/VitDeck/Validator/Tests/F01_TestLightCountLimitRule.cs
@@ -41,7 +41,8 @@ namespace VitDeck.Validator.Test
             var result = rule.Validate(target);
             Assert.That(result.RuleName,Is.EqualTo(string.Format("{0}Lightの個数が制限を超えていることを検出するルール", type)));
             Assert.That(result.Issues.Count, Is.EqualTo(count));
-            Assert.That(result.Issues[0].target.name, Is.EqualTo(string.Format("{0}Light", type)));
+            var targetLight = result.Issues[0].target as Light;
+            Assert.That(targetLight.type, Is.EqualTo(type));
             Assert.That(result.Issues[0].level, Is.EqualTo(IssueLevel.Error));
             //Assert.That(result.Issues[0].message, Is.EqualTo(string.Format("{0} Lightが{1}個を超えています。({2}個)", type, limit, count)));
             //Assert.That(result.Issues[0].solution, Is.EqualTo("別のLightを使用するか、削除して個数を減らして下さい。"));
