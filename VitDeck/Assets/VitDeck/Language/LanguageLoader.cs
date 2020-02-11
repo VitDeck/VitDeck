0e086cdc4bcdf2cdbc77708815a269ff9ef155c9
Sokuhatiku <sokuhatiku@gmail.com>
2020-02-12 05:50:11 +0900

    システム言語設定を読んで表示言語を切り替えるように変更

--- VitDeck/Assets/VitDeck/Language/LanguageLoader.cs
+++ VitDeck/Assets/VitDeck/Language/LanguageLoader.cs
@@ -9,6 +9,27 @@ namespace VitDeck.Language
 {
     public class LanguageLoader
     {
+        private static Dictionary<SystemLanguage, string> languageGUIDs;
+        private static SystemLanguage defaultLanguage = SystemLanguage.English;
+
+        private static Dictionary<SystemLanguage, string> LanguageFileGUIDs
+        {
+            get
+            {
+                if (languageGUIDs != null)
+                {
+                    return languageGUIDs;
+                }
+
+                languageGUIDs = new Dictionary<SystemLanguage, string>
+                {
+                    { SystemLanguage.English, "9c6ce664473c5364f97e0062f8d16c90" },
+                    { SystemLanguage.Japanese, "bca05b8223e02d446a2f3bd3dea0ee44" }
+                };
+
+                return languageGUIDs;
+            }
+        }
 
         [InitializeOnLoadMethod]
         private static void Initialize()
@@ -21,7 +42,32 @@ namespace VitDeck.Language
                 return;
             }
 
-            LocalizedMessage.SetDictionary(settings.language);
+            LanguageDictionary dictionary;
+            if (settings.language == null)
+            {
+                string languageGUID;
+                var currentLanguage = Application.systemLanguage;
+                Debug.Log("Current system language = " + currentLanguage);
+                if (LanguageFileGUIDs.TryGetValue(currentLanguage, out languageGUID))
+                {
+                    Debug.Log("Load LanguageFile which for " + currentLanguage);
+                }
+                else
+                {
+                    Debug.Log("LanguageFile which for current system language is not found. load default LanguageFile.");
+                    languageGUID = LanguageFileGUIDs[defaultLanguage];
+                }
+
+                var defaultLanguagePath = AssetDatabase.GUIDToAssetPath(languageGUID);
+                dictionary = AssetDatabase.LoadAssetAtPath<LanguageDictionary>(defaultLanguagePath);
+            }
+            else
+            {
+                Debug.Log("Load overrided LanguageFile = " + settings.language);
+                dictionary = settings.language;
+            }
+
+            LocalizedMessage.SetDictionary(dictionary);
         }
 
         private static LanguageSettings FindLanguageSettingsInstance()
