038926363014035fdcd1d01d4cc487ba228ac03f
m2wasabi <m2wasabi@gmail.com>
2020-10-14 20:36:53 +0900

    Vket5のRule更新をVket5Udonに移植 追加：【VitDeck】Rigidbodyの使用を許可

--- Assets/VitDeck/Validator/Rules/Vket5Udon/Vket5UdonRuleSetBase.cs
+++ Assets/VitDeck/Validator/Rules/Vket5Udon/Vket5UdonRuleSetBase.cs
@@ -236,7 +236,7 @@ namespace VitDeck.Validator
         private ComponentReference[] GetComponentReferences()
         {
             return new ComponentReference[] {
-                new ComponentReference("Rigidbody", new string[]{"UnityEngine.Rigidbody"}, ValidationLevel.DISALLOW),
+                new ComponentReference("Rigidbody", new string[]{"UnityEngine.Rigidbody"}, ValidationLevel.ALLOW),
                 new ComponentReference("Cloth", new string[]{"UnityEngine.Cloth"}, ValidationLevel.DISALLOW),
                 new ComponentReference("Collider", new string[]{"UnityEngine.SphereCollider", "UnityEngine.BoxCollider", "UnityEngine.SphereCollider", "UnityEngine.CapsuleCollider", "UnityEngine.MeshCollider", "UnityEngine.WheelCollider"}, ValidationLevel.ALLOW),
                 new ComponentReference("Dynamic Bone", new string[]{"DynamicBone"}, ValidationLevel.ALLOW),
