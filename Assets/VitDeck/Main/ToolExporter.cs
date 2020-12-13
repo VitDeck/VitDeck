a58bdb1fa018d3a937b9a59231d5cbd709427b24
100の人 <100@pokemori.jp>
2020-12-13 14:06:42 +0900

    ツール自体のエクスポートで、Temporaryフォルダを除外できていなかったのを修正

--- Assets/VitDeck/Main/ToolExporter.cs
+++ Assets/VitDeck/Main/ToolExporter.cs
@@ -19,8 +19,8 @@ namespace VitDeck.Main
 
         private static readonly Regex IgnorePattern = new Regex(@"^Assets/VitDeck/(
             Main/ToolExporter\.cs
-            |Temporary
-            |.+/Tests/.+|
+            |Temporary(/.+)?
+            |.+/Tests/.+
             |TestUtilities(/.+)?
             |Templates/Sample_template(/.+)?
             |Validator/Rules/Sample(/.+)?
