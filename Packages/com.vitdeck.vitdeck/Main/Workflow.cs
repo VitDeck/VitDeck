f018fc6572972d0242eea4666d31cb6740c63b6e
Sokuhatiku <sokuhatiku@gmail.com>
2023-08-20 18:59:19 +0900

    WorkflowがRuleSetを持てるようにした

--- Packages/com.vitdeck.vitdeck/Main/Workflow.cs
+++ Packages/com.vitdeck.vitdeck/Main/Workflow.cs
@@ -1,10 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
+using VitDeck.Validator;
 
 namespace VitDeck.Main
 {
     [CreateAssetMenu(menuName = "VitDeck/Workflow")]
-    public class Workflow : ScriptableObject
+    public class Workflow : ScriptableObject, ISerializationCallbackReceiver
     {
-        
+        [SerializeField, HideInInspector] private string ruleSetTypeFullName;
+        private IRuleSet ruleSet;
+
+        public void OnBeforeSerialize()
+        {
+            ruleSetTypeFullName = ruleSet == null ? "" : ruleSet.GetType().FullName;
+        }
+
+        public void OnAfterDeserialize()
+        {
+            ruleSet = GetRuleSetInstance(ruleSetTypeFullName);
+        }
+
+        private static IRuleSet GetRuleSetInstance(string fullName)
+        {
+            if (string.IsNullOrEmpty(fullName))
+            {
+                return null;
+            }
+
+            var type = Type.GetType(fullName);
+            if (type == null)
+            {
+                Debug.LogError($"{fullName}型のルールセットがプロジェクト内に定義されていません。");
+                return null;
+            }
+
+            return Activator.CreateInstance(type) as IRuleSet;
+        }
     }
 }
