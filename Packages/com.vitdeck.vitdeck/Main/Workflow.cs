ed49d3511e3726c7c60c38a405fec750a06926bc
Sokuhatiku <sokuhatiku@gmail.com>
2023-08-28 00:50:05 +0900

    ValidatorのRuleSet関連のメソッドを使うようにした

--- Packages/com.vitdeck.vitdeck/Main/Workflow.cs
+++ Packages/com.vitdeck.vitdeck/Main/Workflow.cs
@@ -8,38 +8,9 @@ namespace VitDeck.Main
     /// <summary>
     /// VitDeckを使った一連の作業の流れを定義しているScriptableObject
     /// </summary>
-    public class Workflow : ScriptableObject, ISerializationCallbackReceiver
+    public class Workflow : ScriptableObject
     {
-        
-        [SerializeField, RuleSetType] private string ruleSet;
-        private IRuleSet deserializedRuleSet;
-        public IRuleSet RuleSet => deserializedRuleSet;
-
-        public void OnBeforeSerialize()
-        {
-            ruleSet = deserializedRuleSet == null ? "" : deserializedRuleSet.GetType().AssemblyQualifiedName;
-        }
-
-        public void OnAfterDeserialize()
-        {
-            deserializedRuleSet = GetRuleSetInstance(ruleSet);
-        }
-
-        private static IRuleSet GetRuleSetInstance(string assemblyQualifiedName)
-        {
-            if (string.IsNullOrEmpty(assemblyQualifiedName))
-            {
-                return null;
-            }
-
-            var type = Type.GetType(assemblyQualifiedName);
-            if (type == null)
-            {
-                Debug.LogError($"'{assemblyQualifiedName}'に対応する型のルールセットがプロジェクト内に定義されていません。");
-                return null;
-            }
-
-            return Activator.CreateInstance(type) as IRuleSet;
-        }
+        [SerializeField, RuleSetPopup] private string ruleSet;
+        public IRuleSet RuleSet => Validator.Validator.GetRuleSet(ruleSet);
     }
 }
