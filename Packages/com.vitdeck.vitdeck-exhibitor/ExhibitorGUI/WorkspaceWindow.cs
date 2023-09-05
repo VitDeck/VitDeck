8067496e8d54baef3ba77b08357e330b22431c8e
Sokuhatiku <sokuhatiku@gmail.com>
2023-09-06 00:49:23 +0900

    出展者向けのGUIパッケージを作成し、Workspaceウィンドウを追加

---
+++ Packages/com.vitdeck.vitdeck-exhibitor/ExhibitorGUI/WorkspaceWindow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VitDeck.Main;
+
+namespace VitDeck.ExhibitorGUI
+{
+    public class WorkspaceWindow : EditorWindow
+    {
+        private Workspace workspace;
+        private Editor workspaceEditor;
+
+        private void OnEnable()
+        {
+            EditorSceneManager.sceneOpened += (scene, mode) =>
+            {
+                Debug.Log("Scene Open");
+                FetchCurrentWorkspace();
+            };
+        }
+
+        [MenuItem("VitDeck/Workspace")]
+        static void Open()
+        {
+            var window = GetWindow<WorkspaceWindow>(false, "Workspace");
+            window.FetchCurrentWorkspace();
+        }
+
+        private void FetchCurrentWorkspace()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            var currentScenePath = currentScene.path;
+            if (Workspace.Find(currentScenePath, out var foundWorkspace))
+            {
+                workspace = foundWorkspace;
+                workspaceEditor = Editor.CreateEditor(workspace);
+            }
+            else
+            {
+                workspace = null;
+                DestroyImmediate(workspaceEditor);
+                workspaceEditor = null;
+            }
+        }
+
+        private void OnGUI()
+        {
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.ObjectField("Workspace", workspace, typeof(Workspace), false);
+            EditorGUI.EndDisabledGroup();
+
+            if (workspace == null)
+            {
+                EditorGUILayout.HelpBox("To enable the Workspace window, open a scene that exists within the workspace.", MessageType.Info);
+                return;
+            }
+
+            workspaceEditor.OnInspectorGUI();
+        }
+    }
+}
--- Packages/com.vitdeck.vitdeck/Main/Workspace.cs
+++ Packages/com.vitdeck.vitdeck/Main/Workspace.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
 
 namespace VitDeck.Main
 {
@@ -10,10 +12,40 @@ namespace VitDeck.Main
     public class Workspace : ScriptableObject
     {
         [SerializeField] private Workflow workflow;
-        
+
         public string GetFolderPath()
         {
             return System.IO.Path.GetDirectoryName(UnityEditor.AssetDatabase.GetAssetPath(this));
+        }
+
+        /// <summary>
+        /// 与えられたパスを管理下に持つWorkspaceを探して返す。
+        /// </summary>
+        /// <param name="folderPath">対象のパス</param>
+        /// <param name="workspace">発見したWorkspace</param>
+        /// <returns>Workspaceが見つかればtrue,そうでなければfalse</returns>
+        public static bool Find(string folderPath, out Workspace workspace)
+        {
+            var candidateWorkspacePath = AssetDatabase.FindAssets("t:" + typeof(Workspace).FullName)
+                .Select(AssetDatabase.GUIDToAssetPath)
+                .Where(path =>
+                {
+                    var directoryName = System.IO.Path.GetDirectoryName(path)?.Replace('\\', '/');
+                    return !string.IsNullOrEmpty(directoryName) &&
+                           folderPath.StartsWith(directoryName);
+                })
+                .OrderBy(path => path.Length)
+                .LastOrDefault();
+
+            if (string.IsNullOrEmpty(candidateWorkspacePath))
+            {
+                workspace = default;
+                return false;
+            }
+
+            var candidateWorkspace = AssetDatabase.LoadAssetAtPath<Workspace>(candidateWorkspacePath);
+            workspace = candidateWorkspace;
+            return workspace != null;
         }
     }
 }
